0b3891382a3d2347f01b01ae6907d5541c159ee9
Dong Hoon Kim <68417711+bdlvhn@users.noreply.github.com>
2021-06-10 14:38:06 +0900

    Time: 136 ms (91.34%), Space: 16.4 MB (24.02%) - LeetHub

---
+++ insertion-sort-list/insertion-sort-list.py
@@ -0,0 +1,13 @@
+class Solution:
+    def insertionSortList(self, head: ListNode) -> ListNode:
+        cur = parent = ListNode(0)
+        while head:
+            while cur.next and cur.next.val < head.val:
+                cur = cur.next
+            
+            cur.next, head.next, head = head, cur.next, head.next
+            
+            if head and cur.val > head.val:
+                cur = parent
+            
+        return parent.next
