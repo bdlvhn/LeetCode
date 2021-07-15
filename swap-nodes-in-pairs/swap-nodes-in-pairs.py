4888908e641ef490032a506c9a9b8998b07bca61
Dong Hoon Kim <68417711+bdlvhn@users.noreply.github.com>
2021-07-15 14:43:26 +0900

    Time: 24 ms (96.03%), Space: 14.2 MB (47.81%) - LeetHub

---
+++ swap-nodes-in-pairs/swap-nodes-in-pairs.py
@@ -0,0 +1,8 @@
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if head and head.next:
+            p = head.next
+            head.next = self.swapPairs(p.next)
+            p.next = head
+            return p
+        return head
