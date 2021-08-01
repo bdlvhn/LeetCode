71251eedad365b39b455a4464faebdc5b49182be
Dong Hoon Kim <68417711+bdlvhn@users.noreply.github.com>
2021-08-01 15:22:43 +0900

    Time: 32 ms (65.22%), Space: 14.3 MB (68.98%) - LeetHub

--- minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.py
+++ minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.py
@@ -1,11 +1,14 @@
 class Solution:
-    prev: int = -1e5
-    result: int = 1e5
+    prev: int = -sys.maxsize
+    result: int = sys.maxsize
     def minDiffInBST(self, root: TreeNode) -> int:
         if root.left:
             self.minDiffInBST(root.left)
+
         self.result = min(self.result, root.val - self.prev)
         self.prev = root.val
+
         if root.right:
             self.minDiffInBST(root.right)
+
         return self.result
