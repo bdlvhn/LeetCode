18c070633214c25ec416e55fcc1d203694d24c86
Dong Hoon Kim <68417711+bdlvhn@users.noreply.github.com>
2021-08-05 14:02:40 +0900

    Time: 48 ms (68.78%), Space: 14.3 MB (99.77%) - LeetHub

--- valid-anagram/valid-anagram.py
+++ valid-anagram/valid-anagram.py
@@ -1,3 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        dict_s = collections.defaultdict(int)
+        dict_t = collections.defaultdict(int)
+        
+        for char_s in s:
+            dict_s[char_s] += 1
+        for char_t in t:
+            dict_t[char_t] += 1
+        
+        return dict_s == dict_t
