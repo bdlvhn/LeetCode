5a8d3b69b90321145b593a4d7919b8019eee0c03
Dong Hoon Kim <68417711+bdlvhn@users.noreply.github.com>
2021-08-07 19:27:46 +0900

    Time: 28 ms (78.25%), Space: 14.1 MB (91.80%) - LeetHub

--- sum-of-two-integers/sum-of-two-integers.py
+++ sum-of-two-integers/sum-of-two-integers.py
@@ -1,30 +1,9 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
-        # 전가산기 구현
         MASK = 0xFFFFFFFF
         INT_MAX = 0x7FFFFFFF
-        
-        a_bin = bin(a & MASK)[2:].zfill(32)
-        b_bin = bin(b & MASK)[2:].zfill(32)
-        
-        result = []
-        carry = 0
-        sum = 0
-        for i in range(32):
-            A = int(a_bin[31 - i])
-            B = int(b_bin[31 - i])
-            
-            Q1 = A & B
-            Q2 = A ^ B
-            Q3 = Q2 & carry
-            sum = carry ^ Q2
-            carry = Q1 | Q3
-            
-            result.append(str(sum))
-        if carry == 1:
-            result.append('1')
-        
-        result = int(''.join(result[::-1]),2) & MASK
-        if result > INT_MAX:
-            result = ~(result ^ MASK)
-        return result
+        while b != 0:
+            a, b = (a ^ b) & MASK, ((a & b) << 1) & MASK
+        if a > INT_MAX:
+            a = ~(a ^ MASK)
+        return a
