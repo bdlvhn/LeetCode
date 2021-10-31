961420664852ee4615fca560a007b3760eecaef0
Dong Hoon Kim <68417711+bdlvhn@users.noreply.github.com>
2021-10-31 17:21:48 +0900

    Time: 148 ms (64.95%), Space: 14.4 MB (92.58%) - LeetHub

--- max-area-of-island/max-area-of-island.py
+++ max-area-of-island/max-area-of-island.py
@@ -1,23 +1,28 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
+        maxArea = 0
 
-        def dfs(x: int, y: int, area: int) -> int:
-            if grid[x][y] == 0:
-                return area
-
-            grid[x][y] = 0
-            area += 1
-            dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
-            for i in range(4):
-                nx, ny = x + dx[i], y + dy[i]
-                if 0 <= nx < m and 0 <= ny < n:
-                    area = dfs(nx, ny, area)
-            return area
-
-        answer = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 1:
-                    answer = max(answer, dfs(i, j, 0))
-        return answer
+                    maxArea = max(maxArea, self.findArea(grid, i, j))
+
+        return maxArea
+
+    def findArea(self, board: List[List[int]], i: int, j: int) -> int:
+        queue = collections.deque()
+        queue.append((i, j))
+
+        area = 0
+        dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
+        while queue:
+            x, y = queue.popleft()
+            if board[x][y] != 1: continue
+            board[x][y] = 0
+            area += 1
+            for i in range(4):
+                nx, ny = x + dx[i], y + dy[i]
+                if (0 <= nx < len(board)) and (0 <= ny < len(board[0])):
+                    queue.append((nx, ny))
+        return area
