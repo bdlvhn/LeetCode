97ed646bc502664115cc1312327e8a6a67bb5f37
Dong Hoon Kim <68417711+bdlvhn@users.noreply.github.com>
2021-11-17 17:04:29 +0900

    Time: 1 ms (99.72%), Space: 38.9 MB (96.40%) - LeetHub

--- subsets-ii/subsets-ii.java
+++ subsets-ii/subsets-ii.java
@@ -1,22 +1,23 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        result.add(new ArrayList<>());
-
-        Arrays.sort(nums);
-        for (int num : nums) {
-            List<List<Integer>> newSubset = new ArrayList<>();
-
-            for (List<Integer> elm : result) {
-                newSubset.add(new ArrayList<>(elm) {{
-                    add(num);
-                }});
-            }
-
-            for (List<Integer> elm : newSubset) {
-                if (!result.contains(elm)) result.add(elm);
-            }
-        }
-        return result;
+    Arrays.sort(nums);
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> each = new ArrayList<>();
+    helper(res, each, 0, nums);
+    return res;
+}
+public void helper(List<List<Integer>> res, List<Integer> each, int pos, int[] n) {
+    if (pos <= n.length) {
+        res.add(each);
     }
+    int i = pos;
+    while (i < n.length) {
+        each.add(n[i]);
+        helper(res, new ArrayList<>(each), i + 1, n);
+        each.remove(each.size() - 1);
+        i++;
+        while (i < n.length && n[i] == n[i - 1]) {i++;}
+    }
+    return;
+}
 }
